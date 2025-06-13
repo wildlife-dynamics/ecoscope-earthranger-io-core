83983199250982341969b57420d9d3ed855fc454
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2025-06-13 11:31:18 -0400

    add pytest-src script

---
+++ dev/pytest-src.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+python_version=$1
+
+command="pixi run -e test-${python_version} \
+pytest src/ecoscope_earthranger_io_core/ -vv"
+
+shift 1
+if [ -n "$*" ]; then
+    extra_args=$*
+    command="$command $extra_args"
+fi
+
+echo "Running command: $command"
+eval $command
