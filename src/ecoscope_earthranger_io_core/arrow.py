cfb26a434c1a5bb20458522f1215bd3f7c78d959
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2025-06-13 16:45:06 -0400

    observations arrow schema

---
+++ src/ecoscope_earthranger_io_core/arrow.py
@@ -0,0 +1,13 @@
+import geoarrow.pyarrow
+import pyarrow as pa
+
+
+OBSERVATIONS_ARROW_SCHEMA = pa.schema(
+    [
+        ("location", geoarrow.pyarrow.wkb()),
+        ("recorded_at", pa.string()),
+        ("subject_id", pa.string()),
+        ("subject_name", pa.string()),
+        ("subject_subtype_id", pa.string()),
+    ]
+)
