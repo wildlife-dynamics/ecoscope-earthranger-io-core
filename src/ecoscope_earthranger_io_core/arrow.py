66a9a9c7223aab37f07bbf5a3c0dbbfe9723216a
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2025-06-17 14:07:52 -0400

    arrow conversion WIP

--- src/ecoscope_earthranger_io_core/arrow.py
+++ src/ecoscope_earthranger_io_core/arrow.py
@@ -2,7 +2,7 @@ import geoarrow.pyarrow  # type: ignore[import-untyped]
 import pyarrow as pa
 
 
-OBSERVATIONS_ARROW_SCHEMA = pa.schema(
+OBSERVATIONS_EARTHRANGER_ARROW_SCHEMA = pa.schema(
     [
         ("location", geoarrow.pyarrow.wkb()),
         ("recorded_at", pa.string()),
@@ -11,3 +11,30 @@ OBSERVATIONS_ARROW_SCHEMA = pa.schema(
         ("subject_subtype_id", pa.string()),
     ]
 )
+OBSERVATIONS_ECOSCOPE_ARROW_SCHEMA = pa.schema(
+    [
+        ("geometry", geoarrow.pyarrow.wkb()),
+        ("fixtime", pa.string()),
+        ("groupby_col", pa.string()),
+        ("extra__subject__name", pa.string()),
+        ("extra__subject__subject_subtype", pa.string()),
+    ]
+)
+
+
+def to_ecoscope_schema(earthranger_rb: pa.RecordBatch) -> pa.RecordBatch:
+    """Convert an EarthRanger RecordBatch to an Ecoscope RecordBatch."""
+    assert earthranger_rb.schema.equals(OBSERVATIONS_EARTHRANGER_ARROW_SCHEMA), (
+        f"Expected input schema to be:\n {OBSERVATIONS_EARTHRANGER_ARROW_SCHEMA}\n "
+        f"but got:\n {earthranger_rb.schema}"
+    )
+    renamed_columns = earthranger_rb.rename_columns(
+        {
+            "location": "geometry",
+            "subject_id": "groupby_col",
+            "recorded_at": "fixtime",
+            "subject_name": "extra__subject__name",
+            "subject_subtype_id": "extra__subject__subject_subtype",
+        }
+    )
+    return renamed_columns.cast(OBSERVATIONS_ECOSCOPE_ARROW_SCHEMA)
